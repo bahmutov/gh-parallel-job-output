127915fe67b79d8c4d3584e42c87d782b5fd1fbb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-03-14 16:11:39 -0400

    use state

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ console.log({ total: process.env.JOB_TOTAL, index: process.env.JOB_INDEX })
 async function main() {
   if (process.env.JOB_INDEX === '0') {
     console.log('I am the first job!')
-    core.setOutput('jobStatus', 'failed')
+    core.saveState('jobStatus', 'failed')
   } else {
     console.log('I am not the first job!')
     for (let k = 0; k < 10; k += 1) {
       console.log('checking output from first job...', k, '/', 10)
-      const jobStatus = core.getInput('jobStatus')
+      const jobStatus = core.getState('jobStatus')
       console.log({ jobStatus })
       await new Promise((resolve) => setTimeout(resolve, 10_000))
     }
