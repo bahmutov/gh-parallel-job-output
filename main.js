7de373c38f0ec33d54da18f19af930764243175c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-03-14 16:29:52 -0400

    use artifacts

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const core = require('@actions/core')
+const { DefaultArtifactClient } = require('@actions/artifact')
+const fs = require('fs')
+
+const artifactClient = new DefaultArtifactClient()
 
 console.log('Hello World from main.js!')
 console.log({ total: process.env.JOB_TOTAL, index: process.env.JOB_INDEX })
@@ -6,13 +9,19 @@ console.log({ total: process.env.JOB_TOTAL, index: process.env.JOB_INDEX })
 async function main() {
   if (process.env.JOB_INDEX === '0') {
     console.log('I am the first job!')
-    core.saveState('jobStatus', 'failed')
+    fs.writeFileSync('status.json', JSON.stringify({ status: 'done' }))
+    console.log('status.json written!')
+    const { id, size } = await artifactClient.uploadArtifact('status', [
+      'status.json',
+    ])
+    console.log('status.json uploaded!', { id, size })
   } else {
+    // how to read the status from other jobs?
     console.log('I am not the first job!')
     for (let k = 0; k < 10; k += 1) {
       console.log('checking output from first job...', k, '/', 10)
-      const jobStatus = core.getState('jobStatus')
-      console.log({ jobStatus })
+      const artifact = await artifactClient.getArtifact('status')
+      console.log({ artifact })
       await new Promise((resolve) => setTimeout(resolve, 10_000))
     }
   }
